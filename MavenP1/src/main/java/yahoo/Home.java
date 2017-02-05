caa6f7d130c42663ddb995fb1a741bc5e03d6f2c
venkatgn <venkatadrin.g@gmail.com>
2017-02-05 12:42:20 +0530

    3rd time by tester2

--- MavenP1/src/main/java/yahoo/Home.java
+++ MavenP1/src/main/java/yahoo/Home.java
@@ -4,6 +4,10 @@ public class Home
 {
   public void f1()
   {
-	  System.out.println("hello");
+	  System.out.println("hello");	  
+  }
+  public void f2()
+  {
+	  System.out.println("testing");
   }
 }
